80b4f859562fae4ffa292af1101bbc422d75ac07
JVillegas <JVillegas@jim>
2015-06-28 12:46:32 -0500

    prueba

--- prueba/src/main/principal.java
+++ prueba/src/main/principal.java
@@ -5,7 +5,7 @@ public class principal {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("hola mundo");
+		System.out.println("hola mundo  prueba miercoles");
 
 	}
 
